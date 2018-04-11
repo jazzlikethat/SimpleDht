7c7c02dd12f9135b5c1d5b8c0b6201bc640d06c2
Pradeep Kumar Aitha <pradeep.aitha11@gmail.com>
2018-04-11 18:33:10 -0400

    single AVD

--- app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
@@ -1,15 +1,66 @@
 package edu.buffalo.cse.cse486586.simpledht;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Formatter;
-
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.TextView;
 
 public class SimpleDhtProvider extends ContentProvider {
+
+    static final String TAG = SimpleDhtProvider.class.getSimpleName();
+
+    ArrayList<String> REMOTE_PORTS = new ArrayList<String>(Arrays.asList("11108", "11112", "11116", "11120", "11124"));
+    static final int SERVER_PORT = 10000;
+    String portString;
+    String myPort;
+
+    private static final String KEY_FIELD = "key";
+    private static final String VALUE_FIELD = "value";
+
+    private String prev_id;
+    private String next_id;
+
+    ArrayList<AVD> AVD_List = new ArrayList<AVD>(5);
+
+    public class AVD implements Comparable<AVD> {
+
+        public String my_id;
+        public String hashValue;
+
+        public AVD(String my_id, String hashValue) {
+            this.my_id     = my_id;
+            this.hashValue = hashValue;
+        }
+
+        @Override
+        public int compareTo(AVD avd) {
+            return this.hashValue.compareTo(avd.hashValue);
+        }
+    }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
@@ -26,19 +77,252 @@ public class SimpleDhtProvider extends ContentProvider {
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         // TODO Auto-generated method stub
-        return null;
+
+        String filename = values.getAsString(KEY_FIELD);
+        String content =  values.getAsString(VALUE_FIELD) + "\n";
+
+        try {
+            if (portString.equals(prev_id) && portString.equals(next_id)) {
+                FileOutputStream outputStream = getContext().openFileOutput(filename, Context.MODE_PRIVATE);
+                outputStream.write(content.getBytes());
+                outputStream.close();
+            }
+            else if ((genHash(prev_id).compareTo(genHash(portString)) > 0 && genHash(filename).compareTo(genHash(portString)) <= 0) || (genHash(prev_id).compareTo(genHash(portString)) > 0 && genHash(filename).compareTo(genHash(prev_id)) > 0)){
+                FileOutputStream outputStream = getContext().openFileOutput(filename, Context.MODE_PRIVATE);
+                outputStream.write(content.getBytes());
+                outputStream.close();
+            }
+            else if (genHash(filename).compareTo(genHash(portString)) > 0 || genHash(filename).compareTo(genHash(prev_id)) <= 0){
+                try {
+                    String REMOTE_PORT = String.valueOf((Integer.parseInt(next_id) * 2));
+                    Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
+                            Integer.parseInt(REMOTE_PORT));
+
+                    Log.d(TAG, "Sending insert to other AVD");
+                    String msgToSend = portString + "###" + "insert" + "###" + filename + "###" + content;
+
+                    OutputStream outputStream = socket.getOutputStream();
+                    PrintWriter printWriter = new PrintWriter(outputStream, true);
+                    printWriter.print(msgToSend);
+                    printWriter.flush();
+
+                    socket.close();
+                } catch (UnknownHostException e) {
+                    Log.e(TAG, "ClientTask UnknownHostException");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "ClientTask socket IOException");
+                }
+            }
+            else {
+                FileOutputStream outputStream = getContext().openFileOutput(filename, Context.MODE_PRIVATE);
+                outputStream.write(content.getBytes());
+                outputStream.close();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, "File write failed");
+        }
+
+        return uri;
     }
 
     @Override
     public boolean onCreate() {
-        // TODO Auto-generated method stub
-        return false;
+
+        TelephonyManager tel = (TelephonyManager) this.getContext().getSystemService(Context.TELEPHONY_SERVICE);
+        portString = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+        prev_id = portString;
+        next_id = portString;
+        myPort = String.valueOf((Integer.parseInt(portString) * 2));
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to create a ServerSocket");
+            return false;
+        }
+
+        new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, portString, "addNewAVD", myPort);
+        return true;
+    }
+
+    public Void updateAVDNeighbours(String REMOTE_PORT, String prev_id, String next_id){
+        try {
+            Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
+                    Integer.parseInt(REMOTE_PORT));
+
+            String msgToSend = REMOTE_PORT + "###" + "updateNeighbours" + "###" + prev_id + "###" + next_id;
+
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter printWriter = new PrintWriter(outputStream, true);
+            printWriter.print(msgToSend);
+            printWriter.flush();
+
+            socket.close();
+        } catch (UnknownHostException e) {
+            Log.e(TAG, "ClientTask UnknownHostException");
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "ClientTask socket IOException");
+        }
+        return null;
+    }
+
+
+
+    public Void handleAddNewAvd(String[] msg_split){
+        try {
+            String hashValue = genHash(msg_split[0]);
+            AVD avd = new AVD(msg_split[0], hashValue);
+            AVD_List.add(avd);
+            Collections.sort(AVD_List);
+
+            String REMOTE_PORT;
+
+            if (AVD_List.size() == 1){
+                // Do nothing
+            }
+            else if (AVD_List.size() == 2){
+                REMOTE_PORT = String.valueOf((Integer.parseInt(AVD_List.get(0).my_id) * 2));
+                updateAVDNeighbours(REMOTE_PORT, AVD_List.get(1).my_id, AVD_List.get(1).my_id);
+                REMOTE_PORT = String.valueOf((Integer.parseInt(AVD_List.get(1).my_id) * 2));
+                updateAVDNeighbours(REMOTE_PORT, AVD_List.get(0).my_id, AVD_List.get(0).my_id);
+            }
+            else {
+                int curIndex = AVD_List.indexOf(avd);
+                int prevIndex, nextIndex;
+                if (curIndex == 0){
+                    prevIndex = AVD_List.size() - 1;
+                    nextIndex = curIndex + 1;
+                }
+                else if (curIndex == AVD_List.size() - 1){
+                    prevIndex = curIndex - 1;
+                    nextIndex = 0;
+                }
+                else {
+                    prevIndex = curIndex - 1;
+                    nextIndex = curIndex + 1;
+                }
+
+                AVD prevAVD = AVD_List.get(prevIndex);
+                AVD nextAVD = AVD_List.get(nextIndex);
+
+                REMOTE_PORT = String.valueOf((Integer.parseInt(prevAVD.my_id) * 2));
+                updateAVDNeighbours(REMOTE_PORT, "NULL", avd.my_id);
+
+                REMOTE_PORT = String.valueOf((Integer.parseInt(avd.my_id) * 2));
+                updateAVDNeighbours(REMOTE_PORT, prevAVD.my_id, nextAVD.my_id);
+
+                REMOTE_PORT = String.valueOf((Integer.parseInt(nextAVD.my_id) * 2));
+                updateAVDNeighbours(REMOTE_PORT, avd.my_id, "NULL");
+            }
+        }
+        catch (NoSuchAlgorithmException e){
+            Log.d(TAG, "Could not add the AVD into the chord");
+        }
+        return null;
+    }
+
+    private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
+        @Override
+        protected Void doInBackground(ServerSocket... sockets) {
+            ServerSocket serverSocket = sockets[0];
+            String msgReceived;
+
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    InputStream inputStream = socket.getInputStream();
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    msgReceived = bufferedReader.readLine();
+
+                    String msg_split[] = msgReceived.split("###");
+
+                    if (msg_split[1].equals("addNewAVD")) {
+                        handleAddNewAvd(msg_split);
+                    }
+                    else if (msg_split[1].equals("updateNeighbours")){
+                        prev_id = !msg_split[2].equals("NULL") ? msg_split[2] : prev_id;
+                        next_id = !msg_split[3].equals("NULL") ? msg_split[3] : next_id;
+                        Log.d(TAG, "PrevID: " + prev_id + " CurID: " + portString + " NextID: " + next_id);
+                    }
+                    else if (msg_split[1].equals("insert")){
+                        publishProgress(msgReceived);
+                    }
+
+                    bufferedReader.close();
+                    socket.close();
+                }
+                catch (IOException e) {
+                    Log.e(TAG, "Failed to publish message");
+                }
+            }
+        }
+
+        protected void onProgressUpdate(String...strings) {
+            String msgReceived = strings[0].trim();
+            String msg_split[] = msgReceived.split("###");
+
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(KEY_FIELD, msg_split[2]);
+            contentValues.put(VALUE_FIELD, msg_split[3]);
+            Uri uri = new Uri.Builder().authority("edu.buffalo.cse.cse486586.simpledht.provider").scheme("content").build();
+            insert(uri, contentValues);
+
+            return;
+        }
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
             String sortOrder) {
         // TODO Auto-generated method stub
+
+        try {
+
+            MatrixCursor cursor = new MatrixCursor(new String[]{KEY_FIELD, VALUE_FIELD});
+            FileInputStream fileInputStream;
+            BufferedReader bufferedReader;
+            String content;
+
+            if (selection.equals("@")){
+                String listOfFiles[] = getContext().fileList();
+                for (String S : listOfFiles){
+                    fileInputStream = getContext().openFileInput(S);
+                    bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+                    content = bufferedReader.readLine();
+                    cursor.addRow(new String[] {S, content});
+                }
+                return cursor;
+            }
+            else if (selection.equals("*")){
+                String listOfFiles[] = getContext().fileList();
+                for (String S : listOfFiles){
+                    fileInputStream = getContext().openFileInput(S);
+                    bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+                    content = bufferedReader.readLine();
+                    cursor.addRow(new String[] {S, content});
+                }
+                return cursor;
+            }
+            else {
+                fileInputStream = getContext().openFileInput(selection);
+                bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+                content = bufferedReader.readLine();
+                cursor.addRow(new String[] {selection, content});
+                return cursor;
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "File name failed "+selection);
+            Log.d(TAG, "Reading file failed "+ e.getLocalizedMessage());
+        }
+
+
+
         return null;
     }
 
@@ -56,5 +340,36 @@ public class SimpleDhtProvider extends ContentProvider {
             formatter.format("%02x", b);
         }
         return formatter.toString();
+    }
+
+    private class ClientTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... msgs) {
+
+            String REMOTE_PORT = "11108";
+
+            try {
+                Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
+                        Integer.parseInt(REMOTE_PORT));
+
+                String msgToSend = msgs[0] + "###" + msgs[1];
+
+                OutputStream outputStream = socket.getOutputStream();
+                PrintWriter printWriter = new PrintWriter(outputStream, true);
+                printWriter.print(msgToSend);
+                printWriter.flush();
+
+                socket.close();
+            } catch (UnknownHostException e) {
+                Log.e(TAG, "ClientTask UnknownHostException");
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, "ClientTask socket IOException");
+            }
+
+            return null;
+        }
+
     }
 }
